86e043dae32ffe787a819b93d135c428f1e04f83
Jungwoon <onestarrynights@gmail.com>
2026-06-01 20:56:25 +0900

    Fix path completion test comparison

--- internal/ui/path_completion_test.go
+++ internal/ui/path_completion_test.go
@@ -295,14 +295,5 @@ func mustWriteFile(t *testing.T, path string) {
 }
 
 func equalStrings(a []string, b []string) bool {
-	if len(a) != len(b) {
-		return false
-	}
-	for i := range a {
-		if a[i] != b[i] {
-			return false
-		}
-	}
-
-	return true
+	return slices.Equal(a, b)
 }
